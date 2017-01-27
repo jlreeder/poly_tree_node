66dc3b0360747008e210cae00fb8f688d2df03ba
Jason Reeder <info@jasonreeder.com>
2017-01-27 12:04:26 -0800

    Add #parent

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -1,3 +1,5 @@
+require "byebug"
+
 class PolyTreeNode
 
   attr_accessor :children
@@ -9,9 +11,24 @@ class PolyTreeNode
     @children = []
   end
 
-  def parent=(other_node)
-    @parent = other_node
-    other_node.children << self
+  def parent=(new_parent)
+
+    if new_parent.nil?
+      parent.children.delete(self)
+    elsif new_parent != parent && parent
+      # debugger
+      parent.children.delete(self)
+      new_parent.children << self unless new_parent.children.include?(self)
+    else
+      new_parent.children << self unless new_parent.children.include?(self)
+    end
+    @parent = new_parent
   end
 
+  # def add_child(parent)
+  #   other_node.children << self unless other_node.children.include?(self)
+  # end
+  #
+  # def remove_child
+  # end
 end
